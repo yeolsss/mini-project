cfba709e07836211b2458fbb28804c465b2ad2cc
yeol <yeol8810@gmail.com>
2023-10-05 13:04:14 +0900

    index - 좋아요 부분 firestore 생성 - firestore 데이터 받아오기 - 받아온 데이터 chart에 적용

--- js/index.js
+++ js/index.js
@@ -1,17 +1,48 @@
+/*firebase 연결*/
+import { app } from "./yeol_firebase.js";
+import {
+  getFirestore,
+  collection,
+  setDoc,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  orderBy,
+  deleteDoc,
+  limit,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+
 /*  https://www.chartjs.org/docs/latest/configuration/responsive.html      차트 재설정*/
 
-/*--------------차트 생성 start--------------*/
 const ctx = document.getElementById("myChart");
 
+// firebase 초기 데이터 호출
+// firebase db config
+const db = getFirestore(app);
+const likeRef = collection(db, "member_like");
+
+const likesData = await getDocs(query(likeRef, orderBy("order", "asc")));
+// chart에 필요한 data array
+let nameArr = [];
+let likeArr = [];
+likesData.forEach(data => {
+  const getData = data.data();
+  nameArr.push(getData.name);
+  likeArr.push(getData.like);
+});
+
+// chart color 생성 랜덤 함수
 const randomNum = () => Math.floor(Math.random() * (235 - 52 + 1) + 52);
 
+/*--------------차트 생성 start--------------*/
 new Chart(ctx, {
   type: "bar",
   data: {
-    labels: ["권경열", "최광희", "강지향", "전지현", "정효창"],
+    labels: nameArr,
     datasets: [
       {
-        data: [50, 50, 50, 50, 50],
+        data: likeArr,
         backgroundColor: [
           `rgba(${randomNum()}, ${randomNum()}, ${randomNum()}, 0.3)`,
           `rgba(${randomNum()}, ${randomNum()}, ${randomNum()}, 0.3)`,
