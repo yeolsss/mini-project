1ba2b4a112bc5bdb5f4d70544fce4429db9e0c92
hyochang <jhc729@naver.com>
2023-10-07 17:14:43 +0900

    멤버카드 애니메이션 등록

--- js/index.js
+++ js/index.js
@@ -34,6 +34,19 @@ fadeEls.forEach(function (fadeEls, index) {
 });
 /*--------------헤더 애니메이션 효과 end--------------*/
 
+/*--------------멤버카드 스크롤 애니메이션 효과 start--------------*/
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    // 검사할 장면(Scene)을 추가
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.7, // 화면의 80% 지점에서 보여짐 여부 감시
+  })
+    .setClassToggle(spyEl, "show") // 요소가 화면에서 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!!)
+});
+/*--------------멤버카드 스크롤 애니메이션 효과 end--------------*/
+
 const getLikes = async () => {
   const likesData = await getDocs(query(likeRef, orderBy("order", "asc")));
   nameArr = [];
